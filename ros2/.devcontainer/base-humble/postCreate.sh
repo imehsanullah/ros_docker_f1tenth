75dac80c680501787d43fbe0cb7e8486085f9772
Ehsan Ullah <84843455+imehsanullah@users.noreply.github.com>
2025-03-01 17:17:35 +0100

    Update postCreate.sh

--- ros2/.devcontainer/base-humble/postCreate.sh
+++ ros2/.devcontainer/base-humble/postCreate.sh
@@ -8,4 +8,5 @@ git clone https://github.com/imehsanullah/f1tenth_ws.git
 sudo rosdep update
 sudo rosdep install --from-paths /home/$(whoami)/ws/src --ignore-src -y
 sudo chown -R $(whoami) /home/$(whoami)/ws/
+sudo apt install ros-humble-asio-cmake-module
 colcon build #--cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
